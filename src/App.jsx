432025a15a26d9d63490179d74359435a6aa985f
Eduardo Ureta <eduardo.uretaca@gmail.com>
2023-07-18 18:06:54 -0400

    agregado hasta clase 9

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,54 @@ import { BotonBienvenida } from './componentes/BotonBienvenida'
 
 import '../src/style/style.css'
 import './App.css'
+import { PokeApi } from './componentes/PokeApi'
+import { PokeList } from './componentes/PokeList'
+import { PokeCard } from './componentes/PokeCard'
+import { Contacto } from './componentes/contacto'
+
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import { ItemDetailContainer } from './componentes/ItemDetailContainer'
+
 
 
 
 function App() {
 
   return (
-    <div>
-      <Header></Header>
 
-      <ItemListContainer saludo={"Tienda Online de bebidas y alcohol"}></ItemListContainer>
-    </div>
+    <BrowserRouter>
+    {/* Controlo la navegabilidad */}
+
+      <div>
+        <Routes>
+          <Route path="/PokeApi" element={<Header poke/>}/>
+          <Route path="*" element={<Header/>}/>
+        </Routes>
+
+        <Routes>
+          <Route path="/" element={<ItemListContainer saludo={"Tienda Online de bebidas y alcohol"}></ItemListContainer>}/>
+          <Route path="/PokeApi" element={<PokeApi/>}></Route>
+          {/* <Route path="*" element={<h2>Página no encontrada - Error 404</h2>}></Route> */}
+          <Route path="*" element={<Navigate to="/"/>}></Route>
+            {/* Puedo definir si quiero usar un comodin para las rutas
+            que no existen, o si redirecciono a otra que si existe,
+            esto cuando se ingresa a un path que no existe */}
+    
+          <Route exact path="/productos/:categoryID" element={<ItemListContainer/>}></Route>
+          {/* El : significa que es un valor dinámico */}
+
+          <Route exact path="/detail/:itemId" element={<ItemDetailContainer/>}></Route>
+          
+        </Routes>
+
+
+        {/* <PokeList/>
+        <PokeCard/> */}
+
+        <Contacto/>
+
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -3,9 +3,12 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { CartWidget } from './CartWidget';
 
-export function Header() {
+// Para los enlances de navegación
+import {Link, NavLink} from 'react-router-dom'
+
+export function Header({poke}) {
   return (
-    <Navbar expand="lg" className="bg-dark">
+    <Navbar expand="lg" className={poke ? "bg-success" : "bg-black"}>
       <Container>
         <img width="64" height="64" src="https://img.icons8.com/external-flat-icons-pack-pongsakorn-tan/64/external-alcohol-supermarket-flat-icons-pack-pongsakorn-tan.png" alt="external-alcohol-supermarket-flat-icons-pack-pongsakorn-tan"/>
         <Navbar.Brand href="#home" className='text-warning'>Botillería CL</Navbar.Brand>
@@ -13,11 +16,12 @@ export function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" className='bg-warning'/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#" className='text-light text-center'>Home</Nav.Link>
-            <Nav.Link href="#" className='text-light text-center'>Productos</Nav.Link>
-            <Nav.Link href="#" className='text-light text-center'>Promociones</Nav.Link>
-            <Nav.Link href="#" className='text-light text-center'>¿Cómo preparar tu trago?</Nav.Link>
-            <Nav.Link href="#" className='text-light text-center'>Contacto</Nav.Link>
+            <NavLink to="/" className='text-light text-center'>Home</NavLink>
+            <NavLink to="/PokeApi" className='text-light text-center'>PokeApi</NavLink>
+            <NavLink to="/productos/carniceria" className='text-light text-center'>Carnicería</NavLink>
+            <NavLink to="/productos/verduleria" className='text-light text-center'>Verdulería</NavLink>
+            <NavLink to="/productos/panaderia" className='text-light text-center'>Panadería</NavLink>
+            <NavLink to="/productos/perfumeria" className='text-light text-center'>Perfumería</NavLink>
             <CartWidget></CartWidget>
           </Nav>
         </Navbar.Collapse>
